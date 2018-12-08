dce259283665f84d28ff2f3b5133d61b0c94ebae
Silveste <silveste@mailbox.org>
2018-12-08 15:34:27 +0000

    set frontend

---
+++ public/app.js
@@ -0,0 +1,25 @@
+/*
+This app has been developed by Antonio Fernandez to create a data visualization
+for the module CA682 Data Management and Visualisation of GCAI1 Grad Cert in Artificial
+Intelligence  course (2018/2019)
+*/
+
+/*global d3*/
+document.addEventListener('DOMContentLoaded', function() {
+  // Extract the width and height that was computed by CSS.
+  var chartDiv = document.getElementById('container');
+  var width = chartDiv.clientWidth;
+  var height = chartDiv.clientHeight;
+
+  //Set the map image into the DOM
+  d3.svg('map.svg')
+    .then(svg => {
+      let importedNode = document.importNode(svg.documentElement, true);
+      chartDiv.appendChild(importedNode);
+    });
+
+  //AJAX request
+  d3.json('/api')
+    .then(data => console.log(data))
+    .catch(err => console.log(err));
+});
