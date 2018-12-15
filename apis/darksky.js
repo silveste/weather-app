5b11c4db934b074a07ef0d250bb70e9fea6082ff
Silveste <silveste@mailbox.org>
2018-12-15 12:00:50 +0000

    Add cache to darksky API

--- apis/darksky.js
+++ apis/darksky.js
@@ -1,7 +1,8 @@
 // Handlers for requests to the weather API_KEY */
 const request = require('request-promise-native');
 const api_url = 'https://api.darksky.net/forecast';
-
+const fs = require('fs');
+var data = {};
 /*
 getWeather returns an object with weather information,
 requires 2 parameters:
@@ -9,12 +10,46 @@ requires 2 parameters:
 -time: time string formated as UNIX time
 */
 const getWeather = function(loc){
+  let reqResponse;
+  try {
+    let fileContent = fs.readFileSync('./weather.json');
+    data = JSON.parse(fileContent);
+    reqResponse = data[`${loc.latitude}-${loc.longitude}`];
+  } catch (err) {
+    console.log(err);
+  }
+  if(reqResponse){
+    console.log('Cached weather data');
+    return new Promise((resolve) => {
+      resolve(reqResponse);
+    });
+  } else {
+    console.log('Weather data from API');
+    let options = {
+      uri: `${api_url}/${process.env.API_KEY}/${loc.latitude},${loc.longitude}?exclude=currently,minutely,hourly,alerts,flags`,
+      json: true
+    };
+    let reqPromise = request(options);
+    reqPromise.then(d => {
+      data[`${loc.latitude}-${loc.longitude}`] = d;
+      fs.writeFile('./weather.json', JSON.stringify(data), 'utf8', (err) => {
+        if(err){
+          console.log(err);
+        }
+      });
+    });
+    return reqPromise;
+  }
+};
+
+// The following code deesn't use a cache file to store results
+/*
+const getWeather = function(loc){
   let options = {
     uri: `${api_url}/${process.env.API_KEY}/${loc.latitude},${loc.longitude}?exclude=currently,minutely,hourly,alerts,flags`,
     json: true
   };
   return request(options);
-
 };
-
+*/
 module.exports = getWeather;
