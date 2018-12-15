9c5937fdc278c4025cd06626d4944c73b2504fab
Silveste <silveste@mailbox.org>
2018-12-15 12:11:19 +0000

    Fix issue with Date: App date didn match API date

--- public/app.js
+++ public/app.js
@@ -15,11 +15,13 @@ var irelandSVG = {
   }
 };
 
-//Define today's date in seconds from 00:00:00 UTC
+//Define today and day showed in document;
 var date = {
-  day: new Date(),
-  today: new Date()
+  day: null,
+  today:null
 };
+//Define object that contains all Data
+var mainData = {};
 
 //Define an array with Ireland main cities
 const mainCities = ['Armagh', 'Athlone', 'Belfast', 'Cork', 'Derry', 'Donegal','Drogheda', 'Dublin', 'Galway',
@@ -31,11 +33,6 @@ const canvasWidth = canvas.clientWidth;
 const canvasHeight = canvas.clientHeight;
 
 document.addEventListener('DOMContentLoaded', function() {
-  //Set Today's Date
-  document.querySelector('#back').onclick = function(){changeDay('setYesterday');};
-  document.querySelector('#forward').onclick = function(){changeDay('setTomorrow');};
-  UpdateDay(date.day);
-
   //Set the map image into the DOM
   d3.xml('assets/map.svg')
     .then(xml => {
@@ -47,7 +44,6 @@ document.addEventListener('DOMContentLoaded', function() {
         .attr('width', canvasWidth)
         .attr('height', canvasHeight)
         .attr('viewBox', xmlSVG.attr('viewBox'));
-
       //Set onclick listener for detailed graphic
       irelandSVG.d3svg.on('click', function(){
         let imgCoords = d3.mouse(this);
@@ -59,53 +55,52 @@ document.addEventListener('DOMContentLoaded', function() {
         //weatherData.then(data => console.log(data));
         //let city = callBackend(`/city/Dublin`);
       });
+
+      //Set Today's Date
+      document.querySelector('#back').onclick = function(){changeDay('setYesterday');};
+      document.querySelector('#forward').onclick = function(){changeDay('setTomorrow');};
+      UpdateDay(new Date());
+
+      //getBulkData gets the data for the for the cities specified in mainCities when
+      //the page load for the first time
+      getBulkData();
     });
-  //getBulkData gets the data for the for the cities specified in mainCities when
-  //the page load for the first time
-  getBulkData();
-  setMainCitiesWeather();
 });
 
 function getBulkData() {
 
   //Get the geo coordinates, img coordinates and weather conditions for every city
-  const promArr = mainCities.map( city => {
+  var promArr = mainCities.map( city => {
 
     //1st call the backend to retrieve cities coordinates from Nominatim API
     let locPromise = callBackend(`/city/${city}`);
 
     //2nd call the backend to retrieve weather data from darksky API
     let weatherPromise = locPromise.then((locData) => {
-      return new Promise((resolve,reject) => {
-        let data =callBackend(`/weather/${locData[0].lat},${locData[0].lon}`)
-          .then( data =>{
-            //console.log(data);
-            return data;
+      return callBackend(`/weather/${locData[0].lat},${locData[0].lon}`)
+        .then(weatherData =>{
+          const promArr = weatherData.daily.data.map((weatherInf) => {
+            return  d3.xml(`assets/${weatherInf.icon}.svg`)
+              .then(xml =>{
+                return xml.documentElement;
+              })
+              .then(icon => {
+                weatherInf.icon = icon;
+                return weatherInf;
+              });
           });
-        resolve(data);
-      });
-    });
-
-    //Set the weather icon and create and object to hold daily information
-    let iconPromise = weatherPromise.then(weatherData =>{
-      const promArr = weatherData.daily.data.map(dailyWeather => {
-        return  d3.xml(`assets/${dailyWeather.icon}.svg`)
-          .then(xml =>{
-            return xml.documentElement;
-          });
-      });
-      return Promise.all(promArr)
-        .then(result =>{
-          return weatherData.daily.data.reduce((acc, weatherInf, i) => {
-            acc[weatherInf.time] = weatherInf;
-            return acc;
-          }, {});
-
+          return Promise.all(promArr)
+            .then(result => {
+              return result.reduce((acc, weatherInf) => {
+                acc[weatherInf.time] = weatherInf;
+                delete acc[weatherInf.time].time;
+                return acc;
+              }, {});
+            });
         });
     });
-
     //Data is returned when both calls have been resolved
-    return Promise.all([locPromise,weatherPromise, iconPromise])
+    return Promise.all([locPromise, weatherPromise])
       .then(result =>{
         let locData = result[0];
         let imgCoords = getImgCoords([locData[0].lat,locData[0].lon]);
@@ -115,34 +110,44 @@ function getBulkData() {
           lon: locData[0].lon,
           x: imgCoords[0],
           y: imgCoords[1],
-          days: result[2]
+          days: result[1]
         };
-        console.log(solved);
+        //console.log(solved); //FOR TESTING PURPOSES
         return solved;
       });
   });
-
-  //Once data is resolved, set the icons in the map
-  Promise.all(promArr).then(res => {
-    //console.log(res); //FOR TESTING PURPOSES
-    /*irelandSVG.d3svg.select('#maproot')
-      .selectAll('svg.icon')
-      .data(res)
-      .enter()
-      .append( d => d.dailyWeather[7].iconImg)
-      .attr('xmlns:svg', null)
-      .attr('xmlns', null)
-      .attr('version',null)
-      .attr('height',250)
-      .attr('width',250)
-      .classed('icon',true)
-      .attr('x', d => d.x - 125)
-      .attr('y', d => d.y - 125);*/
+  Promise.all(promArr).then(res =>{
+    mainData = res;
+    console.log(mainData); //FOR TESTING PURPOSES
+    setIcons();
   });
-
 }
 
-//
+function setIcons(){
+  const currentDay = date.day/1000;
+  //D3 general update pattern
+  let d3Update =   irelandSVG.d3svg.select('#maproot')
+    .selectAll('svg.icon')
+    .remove();
+  //All elements are removed when updating therefore there is no need for exit selector
+
+  let d3Enter = irelandSVG.d3svg.select('#maproot')
+    .selectAll('svg.icon')
+    .data(mainData)
+    .enter();
+
+  d3Enter.merge(d3Update)
+    .append( d => d.days[currentDay].icon)
+    .attr('xmlns:svg', null)
+    .attr('xmlns', null)
+    .attr('version',null)
+    .attr('height',250)
+    .attr('width',250)
+    .classed('icon',true)
+    .attr('x', d => d.x - 125)
+    .attr('y', d => d.y - 125);
+}
+
 //helper that accept an array of image coords [x,y] and returns and array of geo coords [lat,lon]
 function getGeoCoords(imgCoords){
   const {north, south, west, east, imgHeight, imgWidth } = irelandSVG.mapDimensions;
@@ -171,7 +176,12 @@ function getImgCoords(geoCoords){
 
 //Update all DOM elements that depends on date, requires a Date object as a parameter
 function UpdateDay(day){
-  date.day = day;
+  //Day must start at 00:00:00
+  let dayMsec = day.getTime() % 86400000;
+  date.day = new Date(day.getTime() - dayMsec);
+  if(!date.today){ //required when initialising the page
+    date.today = date.day;
+  }
   document.getElementById('date').textContent = day.toLocaleString('en-GB',{
     weekday: 'long',
     year: 'numeric',
@@ -189,6 +199,7 @@ function UpdateDay(day){
   if (diffDays === 7*86400000){
     fwd.disabled = true;
   }
+  setIcons();
 }
 
 function changeDay(action){
